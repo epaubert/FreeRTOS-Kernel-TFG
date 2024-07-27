8dc1d49b53bf1f19188d5d94955051213b475438
epaubert <epaubert97@gmail.com>
2024-07-27 18:50:35 +0200

    Empezado desde 0 con los archivos de LPC2000, crashea menos pero no funciona

--- portable/GCC/ARM7_MC13224V/port.c
+++ portable/GCC/ARM7_MC13224V/port.c
@@ -1,14 +1,47 @@
 /*
  * FreeRTOS Kernel <DEVELOPMENT BRANCH>
- * license and copyright intentionally withheld to promote copying into user
- * code.
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
  */
 
+
+/*-----------------------------------------------------------
+* Implementation of functions defined in portable.h for the ARM7 port.
+*
+* Components that can be compiled to either ARM or THUMB mode are
+* contained in this file.  The ISR routines, which can only be compiled
+* to ARM mode are contained in portISR.c.
+*----------------------------------------------------------*/
+
+
+/* Standard includes. */
+#include <stdlib.h>
+
+/* Scheduler includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-
-#define TickTimer timer_0
-#define TimerIntSource itc_src_tmr
 
 /* Constants required to setup the task context. */
 #define portINITIAL_SPSR                   ( ( StackType_t ) 0x1f ) /* System mode, ARM mode, interrupts enabled. */
@@ -16,51 +49,48 @@
 #define portINSTRUCTION_SIZE               ( ( StackType_t ) 4 )
 #define portNO_CRITICAL_SECTION_NESTING    ( ( StackType_t ) 0 )
 
-// void vTaskDelay(const TickType_t xTicksToDelay) PRIVILEGED_FUNCTION;
-// void vTaskStartScheduler(void) PRIVILEGED_FUNCTION;
-//
-// TaskHandle_t xTaskCreateStatic(TaskFunction_t pxTaskCode, const char *const pcName,
-// 	                const configSTACK_DEPTH_TYPE uxStackDepth,
-// 	                void *const pvParameters, UBaseType_t uxPriority,
-// 	                StackType_t *const puxStackBuffer,
-// 	                StaticTask_t *const pxTaskBuffer) PRIVILEGED_FUNCTION;
+/* Constants required to setup the tick ISR. */
+#define TickTimer timer_0
+#define TimerIntSource itc_src_tmr
 
+/* Constants required to setup the VIC for the tick ISR. */
+#define portTIMER_VIC_CHANNEL              ( ( uint32_t ) 0x0004 )
+#define portTIMER_VIC_CHANNEL_BIT          ( ( uint32_t ) 0x0010 )
+#define portTIMER_VIC_ENABLE               ( ( uint32_t ) 0x0020 )
 
-// needed for the tick interrupt
-__attribute__( ( interrupt("IRQ") ) ) void vTickISR( void );
+/*-----------------------------------------------------------*/
 
-// TODO: ?
-static void prvSetupTimerInterrupt( void ){
-    itc_set_handler(TimerIntSource, vTickISR);
-    itc_enable_interrupt(TimerIntSource);
+/* Setup the timer to generate the tick interrupts. */
+static void prvSetupTimerInterrupt( void );
 
-    TimerInt_Init(TickTimer, configTICK_RATE_HZ);
-}
+/*
+ * The scheduler can only be started from ARM mode, so
+ * vPortISRStartFirstSTask() is defined in portISR.c.
+ */
+extern void vPortISRStartFirstTask( void );
 
-BaseType_t xPortStartScheduler(void) { 
-    // Iniciar el timer que genera las interrupciones.
-    // Las interrupciones ya deberían de estar desactivadas aqui.
-    prvSetupTimerInterrupt();
+/*-----------------------------------------------------------*/
 
-    vPortISRStartFirstTask();
+/*
+ * Initialise the stack of a task to look exactly as if a call to
+ * portSAVE_CONTEXT had been called.
+ *
+ * See header file for description.
+ */
+StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
+                                     TaskFunction_t pxCode,
+                                     void * pvParameters )
+{
+    StackType_t * pxOriginalTOS;
 
-    return pdTRUE; 
-}
+    pxOriginalTOS = pxTopOfStack;
 
-void vPortEndScheduler(void) {
-    // Nothing to do, the scheduler shouldn't end
-}
-
-// TODO:Copied from LPC2000
-StackType_t *pxPortInitialiseStack(StackType_t *pxTopOfStack,
-	                                 TaskFunction_t pxCode, void *pvParameters) {
-	(void)pxTopOfStack;
-	(void)pvParameters;
-	(void)*pxCode;
-
-	StackType_t * pxOriginalTOS = pxTopOfStack;
-
+    /* To ensure asserts in tasks.c don't fail, although in this case the assert
+     * is not really required. */
     pxTopOfStack--;
+
+    /* Setup the initial stack of the task.  The stack is set exactly as
+     * expected by the portRESTORE_CONTEXT() macro. */
 
     /* First on the stack is the return address - which in this case is the
      * start of the task.  The offset is added to make the return address appear
@@ -120,33 +150,41 @@ StackType_t *pxPortInitialiseStack(StackType_t *pxTopOfStack,
      * tasks context. */
     *pxTopOfStack = portNO_CRITICAL_SECTION_NESTING;
 
-
-	return pxTopOfStack;
+    return pxTopOfStack;
 }
+/*-----------------------------------------------------------*/
 
-// void vPortYield(void) __atribute__(NAKED) {
-//     /* Within an IRQ ISR the link register has an offset from the true return
-//      * address, but an SWI ISR does not.  Add the offset manually so the same
-//      * ISR return code can be used in both cases. */
-//     __asm volatile ( "ADD       LR, LR, #4" );
-//
-//     /* Perform the context switch.  First save the context of the current task. */
-//     portSAVE_CONTEXT();
-//
-// 	/* Switch to the highest priority task that is ready to run. */
-// 	 vTaskSwitchContext();
-//
-//     /* Restore the context of the new task. */
-//     portRESTORE_CONTEXT();
-// }
+BaseType_t xPortStartScheduler( void )
+{
+    /* Start the timer that generates the tick ISR.  Interrupts are disabled
+     * here already. */
+    prvSetupTimerInterrupt();
 
-// static void prvTickISR(void) {
-//     /* Interrupts must have been enabled for the ISR to fire, so we have to
-//     * save the context with interrupts enabled. */
-//     /* Maintain the tick count. */
-//     if (xTaskIncrementTick() != pdFALSE) {
-//         /* Switch to the highest priority task that is ready to run. */
-//         vTaskSwitchContext();
-//     }
-//     /* start executing the new task */
-// }
+    /* Start the first task. */
+    vPortISRStartFirstTask();
+
+    /* Should not get here! */
+    return 0;
+}
+/*-----------------------------------------------------------*/
+
+void vPortEndScheduler( void )
+{
+    /* It is unlikely that the ARM port will require this function as there
+     * is nothing to return to.  */
+}
+/*-----------------------------------------------------------*/
+
+/*
+ * Setup the timer 0 to generate the tick interrupts at the required frequency.
+ */
+static void prvSetupTimerInterrupt( void )
+{
+    extern void( vTickISR )( void );
+
+    itc_set_handler(TimerIntSource, vTickISR);
+    itc_enable_interrupt(TimerIntSource);
+
+    TimerInt_Init(TickTimer, configTICK_RATE_HZ);
+
+}
